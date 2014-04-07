4257ea5d3f97eb466d53d34e9c8a81df6f784a38
raballand.romain <raballand.romain@gmail.com>
2014-04-07 16:23:38 +0000

    conception du site

--- meehome-java-mail-front/src/main/webapp/resources/js/meehome-java-mail.js
+++ meehome-java-mail-front/src/main/webapp/resources/js/meehome-java-mail.js
@@ -1,27 +1,27 @@
-function extractUrlParams(query){
-	var t;
-	if(query){
-		t =  query.split('&');
-	} else {
-		t = location.search.substring(1).split('&');
-	}
-	var f = [];
-	for (var i=0; i < t.length; i++){
-		var x = t[ i ].split('=');
-		f[x[0]]=x[1];
-	}
-	return f;
-}
+//function extractUrlParams(query){
+//	var t;
+//	if(query){
+//		t =  query.split('&');
+//	} else {
+//		t = location.search.substring(1).split('&');
+//	}
+//	var f = [];
+//	for (var i=0; i < t.length; i++){
+//		var x = t[ i ].split('=');
+//		f[x[0]]=x[1];
+//	}
+//	return f;
+//}
 
 function load(pageUrl,section){
-//	$('.loading').fadeIn();
+	$('.loading').fadeIn();
 	$.ajax({
 		url: pageUrl,
 		context: document.body,
 		contentType: "application/x-www-form-urlencoded;charset=ISO-8859-15",
 		success: function(response){
 			$(section).html(response);
-//			$('.loading').fadeOut();
+			$('.loading').fadeOut();
   		}
 	});
 }
@@ -29,9 +29,9 @@ function load(pageUrl,section){
 function content(fragment){
 	var pageUrl = getUrl(fragment); 
 	load(pageUrl,"#content");
-	if (pageUrl != window.location) {
-        window.history.pushState({path:pageUrl},'',"?fragment="+fragment);
-    }
+//	if (pageUrl != window.location) {
+//        window.history.pushState({path:pageUrl},'',"?fragment="+fragment);
+//    }
 }
 
 function getUrl(fragment){
@@ -54,14 +54,14 @@ load(getUrl('accueil'),"#content");
 /**
  * Lancé au chargement de la page
  */
-$(document).ready(function(){
-	$(window).load(function () {
-		var parametre = extractUrlParams();
-		var value = parametre['fragment'];
-		if(value != undefined){
-			content(value);
-		} else {
-			content('accueil');
-		}
-	});
-});
+//$(document).ready(function(){
+//	$(window).load(function () {
+//		var parametre = extractUrlParams();
+//		var value = parametre['fragment'];
+//		if(value != undefined){
+//			content(value);
+//		} else {
+//			content('accueil');
+//		}
+//	});
+//});
