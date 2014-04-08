b8143a8bbe5335ee170bb5bd44ba4292e0989236
raballand.romain <raballand.romain@gmail.com>
2014-04-08 16:22:54 +0000

    conception du site

--- meehome-java-mail-front/src/main/webapp/resources/js/meehome-java-mail.js
+++ meehome-java-mail-front/src/main/webapp/resources/js/meehome-java-mail.js
@@ -18,17 +18,6 @@ function load(pageUrl,section){
 }
 
 $(document).ready(function(){
-	content('accueil');
-	$(window).load(function () {
-	
-	tinymce.init({
-	    selector: "textarea",
-	    plugins: [
-	        "advlist autolink lists link image charmap print preview anchor",
-	        "searchreplace visualblocks code fullscreen",
-	        "insertdatetime media table contextmenu paste moxiemanager"
-	    ],
-	    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-	});
-	});
+	load('/meehome-java-mail-webapp/pages/content/template/header.html',"header");
+	load('/meehome-java-mail-webapp/pages/content/template/footer.html',"footer");
 });
