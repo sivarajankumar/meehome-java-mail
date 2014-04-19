ba64184a7c975e87565815dbc07446f0870a6536
raballand.romain@gmail.com <raballand.romain@gmail.com@b9724167-1b59-beee-7da2-b3fe257b994c>
2014-04-19 17:55:00 +0000

    Correction build, exclusion apache-commons pour CollectionUtils 3.2

--- trunk/meehome-java-mail-webapp/src/main/java/fr/meehome/mail/front/rest/ListeDiffusionRestService.java
+++ trunk/meehome-java-mail-webapp/src/main/java/fr/meehome/mail/front/rest/ListeDiffusionRestService.java
@@ -1,12 +1,12 @@
 package fr.meehome.mail.front.rest;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import fr.meehome.mail.front.domain.Personne;
 import fr.meehome.mail.front.services.ListeDiffusionService;
